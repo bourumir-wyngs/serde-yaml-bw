53c0e70a4b5b27f4d00cb7b18d6462d78c36a3b3
Bourumir <bourumir.wyngs@gmail.com>
2025-07-24 13:52:16 +0200

    Revert "Add yaml! macro with compile-time validation (#155)" (#156)
    This reverts commit 57f2fd558ec551084dc5c60501bc0c338f2327e2.

--- serde_yaml_bw_macros/tests/compile.rs
+++
@@ -1,5 +0,0 @@
-#[test]
-fn invalid_yaml_fails() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/invalid.rs");
-}
--- serde_yaml_bw_macros/tests/invalid.rs
+++
@@ -1,5 +0,0 @@
-use serde_yaml_bw_macros::yaml;
-fn main() {
-    let _val = yaml!("invalid: [1, 2");
-}
-
