3e2eb1e3612cb2c166d8630c12ccae0100f3b8ae
Bourumir <bourumir.wyngs@gmail.com>
2025-08-21 12:58:28 +0200

    docs: sync html_root_url with crate version

--- src/lib.rs
+++ src/lib.rs
@@ -115,7 +115,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/serde_yaml_bw/2.1.0")]
+#![doc(html_root_url = "https://docs.rs/serde_yaml_bw/2.1.3")]
 #![deny(missing_docs, unsafe_op_in_unsafe_fn)]
 // Suppressed clippy_pedantic lints
 #![allow(
