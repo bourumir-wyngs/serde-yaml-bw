c7d2bb2c89bf45ec80a2b2c1627f9fb0d4fdc96d
Bourumir <bourumir.wyngs@gmail.com>
2025-09-05 21:38:46 +0200

    refactor: extract alias error helper

--- src/ser.rs
+++ src/ser.rs
@@ -828,6 +828,11 @@ where
     ser: &'a mut Serializer<W>,
 }
 
+#[inline]
+fn alias_must_be_string<T>() -> Result<T> {
+    Err(ser::Error::custom("alias must be a string"))
+}
+
 impl<'a, W> ser::Serializer for AliasHelper<'a, W>
 where
     W: io::Write,
@@ -848,64 +853,64 @@ where
     }
 
     fn serialize_bool(self, _v: bool) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_i8(self, _v: i8) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_i16(self, _v: i16) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_i32(self, _v: i32) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_i64(self, _v: i64) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_i128(self, _v: i128) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_u8(self, _v: u8) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_u16(self, _v: u16) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_u32(self, _v: u32) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_u64(self, _v: u64) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_u128(self, _v: u128) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_f32(self, _v: f32) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_f64(self, _v: f64) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_char(self, _v: char) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_bytes(self, _v: &[u8]) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_some<T>(self, _value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: ?Sized + ser::Serialize,
     {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_unit_variant(
         self,
@@ -913,7 +918,7 @@ where
         _variant_index: u32,
         _variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_newtype_struct<T>(
         self,
@@ -923,7 +928,7 @@ where
     where
         T: ?Sized + ser::Serialize,
     {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_newtype_variant<T>(
         self,
@@ -935,20 +940,20 @@ where
     where
         T: ?Sized + ser::Serialize,
     {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_tuple_struct(
         self,
         _name: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_tuple_variant(
         self,
@@ -957,17 +962,17 @@ where
         _variant: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_struct(
         self,
         _name: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
     fn serialize_struct_variant(
         self,
@@ -976,7 +981,7 @@ where
         _variant: &'static str,
         _len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
-        Err(ser::Error::custom("alias must be a string"))
+        alias_must_be_string()
     }
 }
 
